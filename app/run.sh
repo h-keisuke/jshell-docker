de8d95ce5ddc558540495c7fae2157da63095422
keisuke HANAMURA <khanamura@users.noreply.github.com>
2017-02-21 17:44:54 +0900

    Update run.sh
    delete duplicated -class-path option

--- app/run.sh
+++ app/run.sh
@@ -3,4 +3,4 @@ set -u
 
 java -jar /usr/share/java/ivy.jar -ivy /app/ivy.xml -cache /app/cache \
 	-retrieve "/app/lib/[artifact]-[revision].[ext]" 
-jshell -class-path -class-path $(JARS=(/app/lib/*.jar); IFS=:; echo "${JARS[*]}")
+jshell -class-path $(JARS=(/app/lib/*.jar); IFS=:; echo "${JARS[*]}")
